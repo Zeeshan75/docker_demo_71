edb9717757793134cbacb0c0c63a7fb45911e3fc
P MD ZEESHAN SHEIKH <zeeshanshaik75@gmail.com>
2021-08-31 14:11:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "getting started docker new change the file ........................!!"
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True, port=4000)
